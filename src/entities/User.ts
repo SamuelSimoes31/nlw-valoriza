79b75da266cebcddedc655a20590900bb1f79be4
Samuel Simões <samuelsimoes31@gmail.com>
2021-06-23 16:35:27 -0300

    feat: set first entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,6 +1,30 @@
-import {Entity} from "typeorm";
+import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn} from "typeorm";
+import { v4 as uuid } from 'uuid';
 
-@Entity()
+@Entity("Users")
 export class User {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if(!this.id){
+      this.id = uuid();
+    }
+  }
 
 }
